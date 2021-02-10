9bc8f816f61e56270f2cfa3c3a0396c1175d4eb0
foxamanda2 <fox.amanda2@gmail.com>
2021-02-10 18:54:56 -0500

    New Winner Display and reset

--- public/main.js
+++ public/main.js
@@ -9,8 +9,10 @@ function main() {
       thingClickedOn.textContent = `${scoreTeam1}`
     }
     if (scoreTeam1 === 21) {
-      const name = document.querySelector('.team1 h2')
-      return window.alert(`${name.firstChild.data} Won`)
+      const teamHeader = document.querySelector('.team1 h2')
+      const teamOneName = teamHeader.textContent
+      const winnerView = document.querySelector('.team1 footer')
+      winnerView.textContent = `${teamOneName} Won`
     }
   }
   addT1.addEventListener('click', plus1)
@@ -42,14 +44,16 @@ function main() {
   let scoreTeam2 = 0
   const addT2 = document.querySelector('.team2 fieldset i.add')
   function plus2() {
+    scoreTeam2++
     if (scoreTeam2 <= 21) {
       const thingClickedOn = document.querySelector('.team2 h3')
-      scoreTeam2++
       thingClickedOn.textContent = `${scoreTeam2}`
     }
     if (scoreTeam2 === 21) {
-      const name = document.querySelector('.team2 h2')
-      return window.alert(`${name.firstChild.data} Won`)
+      const teamHeader = document.querySelector('.team2 h2')
+      const teamTwoName = teamHeader.textContent
+      const winnerView = document.querySelector('.team2 footer')
+      winnerView.textContent = `${teamTwoName} Won`
     }
   }
   addT2.addEventListener('click', plus2)
@@ -80,16 +84,24 @@ function main() {
   // Reset score
   const resetGame = document.querySelector('.newGame button')
   function newGame() {
+    // Team 1
     const team1Scores = document.querySelector('.team1 h3')
-    const team2Scores = document.querySelector('.team2 h3')
     const name1Reset = document.querySelector('.team1 h2')
+    const win1Reset = document.querySelector('.team1 footer')
+    // Team2
+    const team2Scores = document.querySelector('.team2 h3')
     const name2Reset = document.querySelector('.team2 h2')
+    const win2Reset = document.querySelector('.team2 footer')
+    // Team 1 Reset
     name1Reset.textContent = 'Team 1'
     team1Scores.textContent = 0
     scoreTeam1 = 0
-    name2Reset.textContent = 'Team 2'
+    win1Reset.textContent = ''
+    // Team 2 Reset
+    name2Reset.empty
     team2Scores.textContent = 0
     scoreTeam2 = 0
+    win2Reset.textContent = ''
   }
   resetGame.addEventListener('click', newGame)
 }
