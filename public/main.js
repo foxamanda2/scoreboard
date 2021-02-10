bddc7c1b327cf0335341ca9a8c59744d27f134f2
foxamanda2 <fox.amanda2@gmail.com>
2021-02-10 17:22:57 -0500

    Reset

--- public/main.js
+++ public/main.js
@@ -3,23 +3,27 @@ function main() {
   let scoreTeam1 = 0
   const addT1 = document.querySelector('.team1 fieldset i.add')
   function plus1() {
-    if (scoreTeam1 < 22) {
+    scoreTeam1++
+    if (scoreTeam1 === 21) {
+      return window.alert(`Team 1 Won`)
+    }
+    if (scoreTeam1 <= 21) {
       const thingClickedOn = document.querySelector('.team1 h3')
-      scoreTeam1++
       thingClickedOn.textContent = `${scoreTeam1}`
-
-      if (scoreTeam1 === 21) {
-        window.alert(`Team 1 Won`)
-      }
     }
   }
   addT1.addEventListener('click', plus1)
   // Team 1 Subtract
   const subtractT1 = document.querySelector('.team1 fieldset i.subtract')
   function subtract1() {
-    const thingClickedOn = document.querySelector('.team1 h3')
-
-    thingClickedOn.textContent = `${scoreTeam1--}`
+    if (scoreTeam1 === 0) {
+      window.alert('You cant lose anymore than 0')
+    }
+    if (scoreTeam1 > 0) {
+      const thingClickedOn = document.querySelector('.team1 h3')
+      scoreTeam1--
+      thingClickedOn.textContent = `${scoreTeam1}`
+    }
   }
   subtractT1.addEventListener('click', subtract1)
   // Team 1 Name
@@ -37,17 +41,27 @@ function main() {
   let scoreTeam2 = 0
   const addT2 = document.querySelector('.team2 fieldset i.add')
   function plus2() {
-    const thingClickedOn = document.querySelector('.team2 h3')
-
-    thingClickedOn.textContent = `${scoreTeam2++}`
+    if (scoreTeam2 === 21) {
+      return window.alert(`Team 1 Won`)
+    }
+    if (scoreTeam2 <= 21) {
+      const thingClickedOn = document.querySelector('.team2 h3')
+      scoreTeam2++
+      thingClickedOn.textContent = `${scoreTeam2}`
+    }
   }
   addT2.addEventListener('click', plus2)
   // Team 2 Subtract
   const subtractT2 = document.querySelector('.team2 fieldset i.subtract')
   function subtract2() {
-    const thingClickedOn = document.querySelector('.team2 h3')
-
-    thingClickedOn.textContent = `${scoreTeam2--}`
+    if (scoreTeam2 === 0) {
+      window.alert('You cant lose anymore than 0')
+    }
+    if (scoreTeam2 > 0) {
+      const thingClickedOn = document.querySelector('.team2 h3')
+      scoreTeam2--
+      thingClickedOn.textContent = `${scoreTeam2}`
+    }
   }
   subtractT2.addEventListener('click', subtract2)
   // Team 2 Name
@@ -60,6 +74,16 @@ function main() {
   document
     .querySelector('.team2 input')
     .addEventListener('input', updateListName2)
+
+  // Reset score
+  const reset = document.querySelector('button.newGame')
+  function newGame() {
+    const team1Scores = document.querySelector('.team1 h3')
+    const team2Scores = document.querySelector('.team2 h3')
+    team1Scores.textContent = 0
+    team2Scores.textContent = 0
+  }
+  reset.addEventListener('click', newGame)
 }
 
 document.addEventListener('DOMContentLoaded', main)
